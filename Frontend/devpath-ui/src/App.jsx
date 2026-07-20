46f51b2bd74208ff26c77dc437bb4ef0116d2aee
gunguncodes <gungunkri865@gmail.com>
2026-07-20 15:29:40 +0530

    chore: configure Tailwind CSS and install React Icons

--- Frontend/devpath-ui/src/App.jsx
+++ Frontend/devpath-ui/src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Greeting from "./components/Greeting";
 import MissionCard from "./components/MissionCard";
 import JourneyPreview from "./components/JourneyPreview";
+import NextMilestone from "./components/NextMilestone";
 
 function App() {
   return (
     <div>
+      <h1 className="text-5xl text-blue-600 font-bold">DevPath</h1>
       <Greeting />
       <MissionCard />
       <JourneyPreview />
+      <NextMilestone />
     </div>
   );
 }
---
+++ Frontend/devpath-ui/src/components/NextMilestone.jsx
@@ -0,0 +1,8 @@
+function NextMilestone() {
+
+    return (
+        <h2>Next Milestone</h2>
+    )
+}
+
+export default NextMilestone;
